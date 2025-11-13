66c86d8c8a9607cb64f1cef18c6ad7aa888d0d01
gjpin <3874515+gjpin@users.noreply.github.com>
2025-11-13 17:15:40 +0000

    remove ConsoleSpanExporter

--- bff2/instrumentation.mjs
+++ bff2/instrumentation.mjs
@@ -1,5 +1,4 @@
 import { NodeSDK } from '@opentelemetry/sdk-node';
-import { ConsoleSpanExporter } from '@opentelemetry/sdk-trace-node';
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
 import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-proto';

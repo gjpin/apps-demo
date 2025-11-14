eceee469f38f42d01481fa7432b2885101e546ad
gjpin <3874515+gjpin@users.noreply.github.com>
2025-11-14 13:54:59 +0000

    add new otel dependencies

--- load-app/instrumentation.mjs
+++ load-app/instrumentation.mjs
@@ -10,23 +10,17 @@ import { ATTR_SERVICE_NAME, ATTR_SERVICE_VERSION} from '@opentelemetry/semantic-
 // Enable diagnostic logging
 // diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
 
-// Set otlp endpoints
-const otlpTracesEndpoint = process.env.OTLP_TRACES_ENDPOINT
-const otlpMetricsEndpoint = process.env.OTLP_METRICS_ENDPOINT
-
 const sdk = new NodeSDK({
   resource: resourceFromAttributes({
     [ATTR_SERVICE_NAME]: process.env.OTEL_SERVICE_NAME,
     [ATTR_SERVICE_VERSION]: process.env.OTEL_SERVICE_VERSION,
   }),
   traceExporter: new OTLPTraceExporter({
-    url: otlpTracesEndpoint,
-    headers: {},
+    url: process.env.OTLP_TRACES_ENDPOINT,
   }),
   metricReader: new PeriodicExportingMetricReader({
     exporter: new OTLPMetricExporter({
-      url: otlpMetricsEndpoint,
-      headers: {},
+      url: process.env.OTLP_METRICS_ENDPOINT,
       concurrencyLimit: 1,
     }),
   }),
